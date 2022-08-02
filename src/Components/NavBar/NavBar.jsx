1bcf41528c73258fce4b2e0b926244800c5af30a
unknown <mbisovskyi@gmail.com>
2022-08-02 13:09:01 -0400

    NavBar.css created and NavBar component connected to App + NavBar Component styled

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar-container">
+      <h2 className="logo-text">
+        Fun<span className="muted-text">Posts</span>
+      </h2>
+    </div>
+  );
+};
+
+export default NavBar;
