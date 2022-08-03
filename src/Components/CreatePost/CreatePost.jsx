11e21062b370b73b99f95cf9974ebc55414ef74d
unknown <mbisovskyi@gmail.com>
2022-08-03 19:03:30 -0400

    assets folder created for DisplayPosts + likes images added to folder + images added to DisplayPosts buttons

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -43,25 +43,6 @@ const CreatePost = (props) => {
         <button type="submit">Submit</button>
       </div>
     </form>
-    // <div className="container">
-    //   <div className="create-post-container">
-    //     <div className="name-post-container">
-    //       <p className="name-title">Name</p>
-    //       <p className="post-title">Post</p>
-    //     </div>
-    //     <div className="text-areas-container">
-    //       <div>
-    //         <input className="name-input-area"></input>
-    //       </div>
-    //       <div>
-    //         <textarea className="post-text-area"></textarea>
-    //       </div>
-    //     </div>
-    //     <div className="button-container">
-    //       <button>Submit</button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -1,4 +1,6 @@
 import "./DisplayPosts.css";
+import thumbUp from "./assets/thumb-up.png";
+import thumbDown from "./assets/thumb-down.png";
 
 const DisplayPosts = (props) => {
   return (
@@ -6,8 +8,22 @@ const DisplayPosts = (props) => {
       {props.parentPost.map((entry, index) => {
         return (
           <div key={index} className="post-entries">
-            <p className="name-text">{entry.name}</p>
-            <p className="post-text">{entry.text}</p>
+            <div className="text-field">
+              <p className="name-text">{entry.name}</p>
+              <p className="post-text">{entry.text}</p>
+            </div>
+            <div className="likes-container">
+              <div className="thumb-up">
+                <button type="submit" className="like-button liked">
+                  <img src={thumbUp} alt="Thumb up" />
+                </button>
+              </div>
+              <div className="thumb-down">
+                <button type="submit" className="dislike-button disliked">
+                  <img src={thumbDown} alt="Thumb down" />
+                </button>
+              </div>
+            </div>
           </div>
         );
       })}
